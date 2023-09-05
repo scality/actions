865f88116b4da82ed03acb2da48bd35b6efcdbd3
Alexandre Lavigne <alexandre.lavigne@scality.com>
2023-09-05 17:36:54 +0200

    tmp

--- xcore/bump_version_pull_request/push_new_version.sh
+++ xcore/bump_version_pull_request/push_new_version.sh
@@ -49,6 +49,8 @@ for branch in ${upper_branches}; do
     echo "Create and push waterfall branch for origin/development/${branch}"
     git checkout -B w/${branch}/feature/bump_version_${NEW_VERSION} origin/development/${branch}
 
+    git log --oneline --graph --all --decorate | cat
+
     # We now we can always use 'ours' as this sould only update the version file for the current branch only
     git merge --strategy=ours --no-edit feature/bump_version_${NEW_VERSION}
 
