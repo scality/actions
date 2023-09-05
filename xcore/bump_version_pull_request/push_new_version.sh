b2b95a53845b19ec3fe5ff76004cd5646ad1347e
Alexandre Lavigne <alexandre.lavigne@scality.com>
2023-09-05 17:48:11 +0200

    tmp

--- xcore/bump_version_pull_request/push_new_version.sh
+++ xcore/bump_version_pull_request/push_new_version.sh
@@ -13,6 +13,9 @@ VERSION=$3
 NEW_VERSION=$4
 git fetch --all
 
+git log --oneline --graph --all --decorate > current_git_graph.txt
+cat current_git_graph.txt
+
 git checkout -b feature/bump_version_${NEW_VERSION} ${REF}
 
 # update version patch level
